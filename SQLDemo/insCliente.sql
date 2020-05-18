65f513dbb446d3dea4192b57d76aaede8dc69249
Diego Melgarejo <diego.melgarejo@microsoft.com>
2020-05-18 19:20:15 -0400

    add sp

---
+++ SQLDemo/insCliente.sql
@@ -0,0 +1,22 @@
+﻿CREATE PROCEDURE insCliente 
+	@id int, @Nombre varchar(50), @Apellido varchar(50), @FechaNacimiento date
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+		INSERT INTO [dbo].[Clientes]
+				   ([Id]
+				   ,[Nombre]
+				   ,[Apellido]
+				   ,[FechaNacimiento])
+			 VALUES
+				   (@id
+				   ,@Nombre
+				   ,@Apellido
+				   ,@FechaNacimiento)
+END
+
+
+
+
