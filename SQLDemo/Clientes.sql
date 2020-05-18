5b445164df94594badbfab19e5c74aa027eb9a24
Diego Melgarejo <diego.melgarejo@microsoft.com>
2020-05-18 19:36:42 -0400

    added documento

--- SQLDemo/Clientes.sql
+++ SQLDemo/Clientes.sql
@@ -3,5 +3,6 @@
 	[Id] INT NOT NULL PRIMARY KEY, 
     [Nombre] VARCHAR(50) NULL, 
     [Apellido] VARCHAR(50) NULL, 
-    [FechaNacimiento] DATE NULL
+    [FechaNacimiento] DATE NULL, 
+    [Documento] VARCHAR(50) NULL
 )
